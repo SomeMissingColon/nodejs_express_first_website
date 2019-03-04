0021f8799ae30214ec123349f911b834beceb927
Thomas Dion <thomasnoidion@gmail.com>
2019-03-04 15:45:54 -0500

    first commit, getting some chills!

---
+++ index.js
@@ -0,0 +1,37 @@
+//importing depedencies
+
+var express = require('express');
+var bodyParser = require('body-parser');
+var path = require('path');
+
+var app = express();
+
+//View engine
+app.set('view engine', 'ejs');
+app.set('views', path.join(__dirname, 'views'));
+//body parser middleware
+
+app.use(bodyParser.json());
+app.use(bodyParser.urlencoded({extended: false}));
+
+//set Static Path
+
+app.use(express.static(path.join(__dirname, 'public')));
+
+//rooting
+
+app.get('/',function(req,res){
+	res.render('index')
+});
+
+app.get('/thomas',function(req,res){
+	res.render('thomas',{
+		today: new Date
+	});
+});
+
+//starting server
+
+app.listen(3000,function(){
+	console.log('this be callback, telling you when the server started')
+});
